8a52651e15a7525af73cece1059efb1844341a79
Gerrit Gogel <gerrit.gogel@student.kit.edu>
2023-04-22 11:09:49 +0200

    make reddit download configurable

--- redditpreview.py
+++ redditpreview.py
@@ -11,8 +11,9 @@ from maubot.handlers import event
 
 class Config(BaseProxyConfig):
     def do_update(self, helper: ConfigUpdateHelper) -> None:
-        helper.copy("reddit.title")
-        helper.copy("reddit.photo")
+        helper.copy("reddit.enabled")
+        helper.copy("reddit.info")
+        helper.copy("reddit.image")
         helper.copy("reddit.video")
         helper.copy("instagram.info")
         helper.copy("instagram.photo")
@@ -69,64 +70,66 @@ class RedditPreviewPlugin(Plugin):
                     await self.client.send_file(evt.room_id, url=uri, info=BaseFileInfo(mimetype=mime_type, size=len(media)), file_name=file_name,file_type=MessageType.VIDEO)
         for url_tup in reddit_pattern.findall(evt.content.body):
             await evt.mark_read()
-            url = ''.join(url_tup).split('?')[0]
-            query_url = quote(url).replace('%3A', ':') + ".json" + "?limit=1"
-            headers = {
-                'User-Agent': 'ggogel/RedditPreviewMaubot'
-            }
-            response = await self.http.request('GET', query_url, headers=headers)
+            if self.config["reddit.enabled"]:
+                url = ''.join(url_tup).split('?')[0]
+                query_url = quote(url).replace('%3A', ':') + ".json" + "?limit=1"
+                headers = {
+                    'User-Agent': 'ggogel/RedditPreviewMaubot'
+                }
+                response = await self.http.request('GET', query_url, headers=headers)
 
-            if response.status != 200:
-                self.log.warning(f"Unexpected status fetching reddit listing {query_url}: {response.status}")
-                return None
-            
-            response_text = await response.read()
-            data = json.loads(response_text.decode())
+                if response.status != 200:
+                    self.log.warning(f"Unexpected status fetching reddit listing {query_url}: {response.status}")
+                    return None
+                
+                response_text = await response.read()
+                data = json.loads(response_text.decode())
 
-            sub = data[0]['data']['children'][0]['data']['subreddit_name_prefixed']
-            title = data[0]['data']['children'][0]['data']['title']
-            name = data[0]['data']['children'][0]['data']['name']
-            
-            await evt.reply(TextMessageEventContent(msgtype=MessageType.TEXT, format=Format.HTML, body=f"{sub}: {title}", formatted_body=f"""<p><b>{sub}: {title}</b></p>"""))
-            
-            # We assume that when this condition is true, the post is either an image or video
-            if 'url_overridden_by_dest' in  data[0]['data']['children'][0]['data']:
-                media_url = data[0]['data']['children'][0]['data']['url_overridden_by_dest']
-                mime_type = mimetypes.guess_type(media_url)[0]
-
-                # Use video fallback URL if original URL has non-standard or no file extension. This is typically the case for "gifs" on imgur and gfycat, which are actually videos.
-                if mime_type == None:
-                    if 'reddit_video' in data[0]['data']['children'][0]['data']['secure_media']:
-                        media_url = data[0]['data']['children'][0]['data']['secure_media']['reddit_video']['fallback_url'].split('?')[0]
-                    else:
-                        media_url = data[0]['data']['children'][0]['data']['preview']['reddit_video_preview']['fallback_url'].split('?')[0]
+                sub = data[0]['data']['children'][0]['data']['subreddit_name_prefixed']
+                title = data[0]['data']['children'][0]['data']['title']
+                name = data[0]['data']['children'][0]['data']['name']
+                
+                if self.config["reddit.info"]:
+                    await evt.reply(TextMessageEventContent(msgtype=MessageType.TEXT, format=Format.HTML, body=f"{sub}: {title}", formatted_body=f"""<p><b>{sub}: {title}</b></p>"""))
+                
+                # We assume that when this condition is true, the post is either an image or video
+                if 'url_overridden_by_dest' in  data[0]['data']['children'][0]['data']:
+                    media_url = data[0]['data']['children'][0]['data']['url_overridden_by_dest']
                     mime_type = mimetypes.guess_type(media_url)[0]
 
-                file_extension = mimetypes.guess_extension(mime_type)
-                file_name = name + file_extension
-                
-                if "image" in mime_type:
-                    response = await self.http.get(media_url)
+                    # Use video fallback URL if original URL has non-standard or no file extension. This is typically the case for "gifs" on imgur and gfycat, which are actually videos.
+                    if mime_type == None:
+                        if 'reddit_video' in data[0]['data']['children'][0]['data']['secure_media']:
+                            media_url = data[0]['data']['children'][0]['data']['secure_media']['reddit_video']['fallback_url'].split('?')[0]
+                        else:
+                            media_url = data[0]['data']['children'][0]['data']['preview']['reddit_video_preview']['fallback_url'].split('?')[0]
+                        mime_type = mimetypes.guess_type(media_url)[0]
 
-                    if response.status != 200:
-                        self.log.warning(f"Unexpected status fetching media {media_url}: {response.status}")
+                    file_extension = mimetypes.guess_extension(mime_type)
+                    file_name = name + file_extension
+                    
+                    if "image" in mime_type and self.config["reddit.image"]:
+                        response = await self.http.get(media_url)
+
+                        if response.status != 200:
+                            self.log.warning(f"Unexpected status fetching media {media_url}: {response.status}")
+                            return None
+
+                        media = await response.read()
+                        uri = await self.client.upload_media(media, mime_type=mime_type, filename=file_name)
+                        await self.client.send_image(evt.room_id, url=uri, file_name=file_name, info=ImageInfo(mimetype='image/jpg'))
+                    elif "video" in mime_type and self.config["reddit.video"]:
+                        audio_url = media_url.replace("DASH_720","DASH_audio")
+                        media_url = "https://sd.rapidsave.com/download.php?permalink={}&video_url={}?source=fallback&audio_url={}?source=fallback".format(url, media_url, audio_url)
+                        response = await self.http.get(media_url)
+
+                        if response.status != 200:
+                            self.log.warning(f"Unexpected status fetching media {media_url}: {response.status}")
+                            return None
+
+                        media = await response.read()
+                        uri = await self.client.upload_media(media, mime_type=mime_type, filename=file_name)
+                        await self.client.send_file(evt.room_id, url=uri, info=BaseFileInfo(mimetype=mime_type, size=len(media)), file_name=file_name,file_type=MessageType.VIDEO)
+                    elif self.config["reddit.image"] or self.config["reddit.video"]:
+                        self.log.warning(f"Unknown media type {query_url}: {mime_type}")
                         return None
-
-                    media = await response.read()
-                    uri = await self.client.upload_media(media, mime_type=mime_type, filename=file_name)
-                    await self.client.send_image(evt.room_id, url=uri, file_name=file_name, info=ImageInfo(mimetype='image/jpg'))
-                elif "video" in mime_type:
-                    audio_url = media_url.replace("DASH_720","DASH_audio")
-                    media_url = "https://sd.rapidsave.com/download.php?permalink={}&video_url={}?source=fallback&audio_url={}?source=fallback".format(url, media_url, audio_url)
-                    response = await self.http.get(media_url)
-
-                    if response.status != 200:
-                        self.log.warning(f"Unexpected status fetching media {media_url}: {response.status}")
-                        return None
-
-                    media = await response.read()
-                    uri = await self.client.upload_media(media, mime_type=mime_type, filename=file_name)
-                    await self.client.send_file(evt.room_id, url=uri, info=BaseFileInfo(mimetype=mime_type, size=len(media)), file_name=file_name,file_type=MessageType.VIDEO)
-                else:
-                    self.log.warning(f"Unknown media type {query_url}: {mime_type}")
-                    return None
